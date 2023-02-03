31594a581958369f65a3a0c0e980b447abb1e781
Your Name <you@example.com>
2023-02-03 10:53:43 +0100

    first commit

---
+++ vidididi.js
@@ -0,0 +1,11 @@
+//domen kao objekat u konzoli
+//console.dir(document)
+//ispise domen dokumenta
+//console.log(document.domain)
+//ime sajta
+//console.log(document.title)
+
+//console.log(document.forms)
+let listItems = document.getElementsByClassName("list-items");
+console.log(listItems)
+listItems[2].textContent="nesto drugo"
